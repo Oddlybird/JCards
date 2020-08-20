2c4a5312ab3f7eee11015d821cc16d50be21c79d
Oddbird <48892307+Oddlybird@users.noreply.github.com>
2020-08-20 00:53:18 -0600

    Updated to ArrayList<Card>

--- Misc.java
+++ Misc.java
@@ -1,45 +1,22 @@
+import java.util.ArrayList;
 
 public class Misc {
 
 	public static void main(String[] args) {
 		// dicecheck();
-		// printdeck();
-		pokerhand();
+		printdeck();
 	}
-
 	
 	// Card.java
-	public static void pokerhand() {
-		// Cause 1 deck of playing cards
-		Card cards = new Card("", "");
-		Card[] deck = cards.deckplayingcard(false);
-		deck = cards.shuffle(deck);
-		// A hand of five cards 
-		Card[] hand = new Card[5];
-		hand = cards.draw(deck, 5);
-		hand = cards.sortvalue(hand);
-		// print the hand, then the deck
-		String handlist = hand[0].name() + ", " + hand[1].name() + ", " + hand[2].name() + ", " + hand[3].name() + ", " + hand[4].name(); 
-		String valulist = hand[0].valuenum() + ", " + hand[1].valuenum() + ", " + hand[2].valuenum() + ", " + hand[3].valuenum() + ", " + hand[4].valuenum(); 
-	    System.out.println(handlist);
-	    System.out.println(valulist);
-	    System.out.println(cards.pokerhand(hand));
-	    System.out.println(" - ");
-		// To print the entire deck in order
-		// for (Card arb : deck) {System.out.println(arb.name());};		
-		// note to self, you still need to shift all blank cards to the bottom o the stack
-	}
-
-	// Card.java
 	public static void printdeck() {
-		// How to initialize Cards as an object
-		Card cards = new Card("", "");	
 		// How to summon a deck of standard playing cards
-		Card[] deck = cards.deckplayingcard(false);
+		ArrayList<Card> deck = Card.deckplayingcard(false);
 		// ...and shuffle it: 
-		deck = cards.shuffle(deck);
+		deck = Card.shuffle(deck);
 		// Print the name of every card in the deck
-		for (Card arb : deck) {System.out.println(arb.name());};		
+		for (int i=0; i<deck.size(); i++) {
+			System.out.println(deck.get(i).name());
+		}
 	}
 	
 	// Dice.java
